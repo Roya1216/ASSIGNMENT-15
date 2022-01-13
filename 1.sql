6ecb8ad789fc633931af5ccfaf242fb3a24e925d
Roya1216 <91912003+Roya1216@users.noreply.github.com>
2022-01-13 17:40:55 +0330

    Add files via upload

---
+++ 1.sql
@@ -0,0 +1,11 @@
+INSERT INTO customers (id,name,city,country)
+VALUES(356,'Roya Tavasoli','Mashhad','iran'),
+(467,'simin karimi','Tabriz','iran'),
+(782,'scarlet mifi','london','england'),
+(9845,'mina bagheri','Tehran','iran');
+
+INSERT INTO products (id,name,price,count)
+VALUES(249,'Tv','700000','20'),
+(2154,'book','2000','6000'),
+(9,'bag','40000','180'),
+(7852,'mobl','5000000','0');
